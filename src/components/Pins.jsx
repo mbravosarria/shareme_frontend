3a5b7f27ec5d1b063a0546a4ef8ae54c4b7cc1a1
mbravosarria <mbravosarria@gmail.com>
2023-03-15 18:01:26 -0400

    fix: extra imports

--- src/components/Pins.jsx
+++ src/components/Pins.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 import { NavBar, Feed, PinDetails, CreatePin, Search } from "./";
 
